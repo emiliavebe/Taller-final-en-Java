523c421e13bce6d356ff285eca14612c61de92b6
emiliavebe <emiliavebe@gmail.com>
2026-09-20 22:47:18 -0500

    inicio del taller final

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*
+
+TITULO: Sistema de getion de personajes
+
+AUTOR: Emilia Veloz
+
+FECHA: 20/09/2026
+
+ENUNCIADO: Una academia de videojuegos necesita un programa para registrar personajes. Todos comparten un
+identificador, un nombre y un nivel, pero cada tipo realiza una acción diferente. El sistema debe guardar los
+objetos en una sola colección, mostrarlos y buscar uno por su identificador.
+
+*/
+
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
